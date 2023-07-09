6e232fbea17dcde564035f840c35b8bd81ef075e
Burit Boonkorn <burit_bk@hotmail.com>
2023-07-10 01:10:19 +0700

    finish sending url request and parsing json response

---
+++ Netflix Clone/Models/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  Netflix Clone
+//
+//  Created by Burit Boonkorn on 10/7/2566 BE.
+//
+
+import Foundation
+
+struct TrendingMoviesResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int
+    let media_type: String?
+    let original_title: String?
+    let poster_path: String?
+    let overview: String?
+    let vote_count: Int?
+    let release_date: String?
+    let vote_average: Double
+}
