a1c237e35faac1233b2d82dd6e2a642a406afdbd
Burit Boonkorn <burit_bk@hotmail.com>
2023-07-13 23:26:02 +0700

    Finish Refactor Model

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -30,7 +30,7 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
         
-        getTrendingMovies()
+        fetchData()
     }
     
     private func configureNavbar() {
@@ -51,14 +51,30 @@ class HomeViewController: UIViewController {
         homeFeedTable.frame = view.bounds
     }
     
-    private func getTrendingMovies() {
-        APICaller.shared.getTrendingMovies { results in
-            switch results {
-                case .success(let movies):
-                    print(movies)
-                case .failure(let error):
-                    print(error)
-            }
+    private func fetchData() {
+//        APICaller.shared.getTrendingMovies { results in
+//            switch results {
+//                case .success(let movies):
+//                    print(movies)
+//                case .failure(let error):
+//                    print(error)
+//            }
+//        }
+        
+//        APICaller.shared.getTrendingTvs { results in
+//            //
+//        }
+        
+//        APICaller.shared.getUpcomingMovies { results in
+//            //
+//        }
+        
+//        APICaller.shared.getPopularMovies { results in
+//            //
+//        }
+        
+        APICaller.shared.getTopRated { results in
+            //
         }
     }
 }
--- Netflix Clone/Models/Title.swift
+++ Netflix Clone/Models/Title.swift
@@ -7,14 +7,15 @@
 
 import Foundation
 
-struct TrendingMoviesResponse: Codable {
-    let results: [Movie]
+struct TrendingTitleResponse: Codable {
+    let results: [Title]
 }
 
-struct Movie: Codable {
+struct Title: Codable {
     let id: Int
     let media_type: String?
     let original_title: String?
+    let original_name: String?
     let poster_path: String?
     let overview: String?
     let vote_count: Int?
