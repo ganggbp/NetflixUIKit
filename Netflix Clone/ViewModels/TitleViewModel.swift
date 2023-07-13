d8916f6322021abfe80b366755d7cfbd4a3f5766
Burit Boonkorn <burit_bk@hotmail.com>
2023-07-14 01:56:44 +0700

    finish creating TitleViewModel

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Burit Boonkorn on 14/7/2566 BE.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
